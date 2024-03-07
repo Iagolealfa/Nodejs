fbae3046ba86752b1b91fbe9826626a6cd1afac9
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2024-03-07 10:16:05 -0300

    Instalando o express e criação do servidor web, aplicado o response para transmitir a mensagem Olá mundo!

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import validator from 'validator';
-
-let name: string = 'iago';
-
-if( validator.isLowercase(name)){
-    console.log(`A string ${name} é toda minúscula`)
-}else{
-    console.log(`A string ${name} não é toda minúscula`)
-}
