1da02de43f11789014a41cbd9069debf94438a19
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2024-03-09 22:55:53 -0300

    Criação de exibição condicional no mustache

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,16 @@ import {Router,Request,Response} from 'express';
 const router = Router();
 
 router.get('/',(req: Request,res: Response)=>{
+    let age: number = 45;
+    let showOld :boolean = false;
+    if (age > 50){
+        showOld = true;
+    }
    
     res.render('home', {
         name : 'Iago',
         lastname : 'Almeida',
+        showOld 
     });
 })
 
