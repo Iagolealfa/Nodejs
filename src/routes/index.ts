dc9ff78b5f85dc4829be6f0861eb58f56a3cf745
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2024-03-09 23:07:03 -0300

    Adicionando exibição de lista simples e lista com objetos(loop) pelo mustache

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,7 +12,17 @@ router.get('/',(req: Request,res: Response)=>{
     res.render('home', {
         name : 'Iago',
         lastname : 'Almeida',
-        showOld 
+        showOld,
+        products : [
+            {title : 'Pizza de frango', price : 50},
+            {title : 'Pizza de calabresa', price : 45},
+            {title : 'Pizza de mussarela', price : 40},
+        ],
+        frasesDoDia:[
+            "Acredite em si mesmo e você será imparável.",
+            "Cada dia é uma nova oportunidade para brilhar.",
+            "A persistência leva ao sucesso."
+        ] 
     });
 })
 
